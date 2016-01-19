1a6197c7547f9ca627e1834dd15c470b1a0cb17b
Jason Lee Ertle <jason.ertle@gmail.com>
2016-01-19 15:57:50 -0800

    Bumped to 0.1.29

--- server/lib/logger.js
+++ server/lib/logger.js
@@ -4,13 +4,13 @@ require('winston-loggly');
 
 //to continue after an exception is caught set exitOnError to false.
 winston.exitOnError = true;
-
-winston.add(winston.transports.Loggly, {
-  token: process.env.LOGGLY_TOKEN,
-  subdomain: process.env.LOGGLY_SUBDOMAIN,
-  tags: ["Winston-NodeJS"],
-  json: true
-});
+//
+// winston.add(winston.transports.Loggly, {
+//   token: process.env.LOGGLY_TOKEN,
+//   subdomain: process.env.LOGGLY_SUBDOMAIN,
+//   tags: ["Winston-NodeJS"],
+//   json: true
+// });
 
 // ? local file useful if network is lost ?
 winston.handleExceptions(new winston.transports.File({
